de347df815e5ee4afa31b7872e62c6d761125f42
MonikaV-06 <monika.vallabhapurapu@gmail.com>
2023-06-27 22:03:45 +0530

    created nacl rules

--- Firewalls.tf
+++ Firewalls.tf
@@ -5,3 +5,27 @@ resource "aws_network_acl" "main-nacl" {
     Name = "main-nacl"
   }
 }
+
+# NACL Rules - Inbound
+resource "aws_network_acl_rule" "main-ingress" {
+  network_acl_id = aws_network_acl.main-nacl.id
+  rule_number    = 100
+  egress         = false
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 0
+  to_port        = 65535
+}
+
+# NACL Rules - outbound
+resource "aws_network_acl_rule" "main-egress" {
+  network_acl_id = aws_network_acl.main-nacl.id
+  rule_number    = 100
+  egress         = true
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 0
+  to_port        = 65535
+}
