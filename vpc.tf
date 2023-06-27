79c9aece2f75e5c0983103471fe5c436e487351f
MonikaV-06 <monika.vallabhapurapu@gmail.com>
2023-06-27 20:17:49 +0530

    created route

--- vpc.tf
+++ vpc.tf
@@ -39,3 +39,10 @@ resource "aws_route_table" "main-rtb" {
     Name = "main-rtb"
   }
 }
+
+# Route for internet
+resource "aws_route" "main-igw-route" {
+  route_table_id         = aws_route_table.main-rtb.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main-igw.id
+}
