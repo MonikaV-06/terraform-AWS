b90a5c412180dbf9166c7041d66274ef26931a77
MonikaV-06 <monika.vallabhapurapu@gmail.com>
2023-06-27 16:53:47 +0530

    created subnet

--- vpc.tf
+++ vpc.tf
@@ -9,3 +9,16 @@ resource "aws_vpc" "main" {
     Name = "main"
   }
 }
+
+
+# SUBNETS
+
+resource "aws_subnet" "main-subnet" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "main-subnet"
+  }
+}
