e144b6bec4f61323c8c75e7a781732e656a08a38
MonikaV-06 <monika.vallabhapurapu@gmail.com>
2023-06-28 15:04:55 +0530

    created web-sg

--- Firewalls.tf
+++ Firewalls.tf
@@ -30,8 +30,18 @@ resource "aws_network_acl_rule" "main-egress" {
   to_port        = 65535
 }
 
-
+# NACL - Subnet - Association
 resource "aws_network_acl_association" "nacl-subnet" {
   network_acl_id = aws_network_acl.main-nacl.id
   subnet_id      = aws_subnet.main-subnet.id
+}
+
+# Security Group
+resource "aws_security_group" "main-web-sg" {
+  name        = "allow-ssh-http"
+  description = "Allow SSH & HTTP traffic"
+  vpc_id      = aws_vpc.main.id
+  tags = {
+    Name = "main-web-sg"
+  }
 }
